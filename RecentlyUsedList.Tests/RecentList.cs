4d7a649789661e4f21bda63f2dae550ae9d92a35
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-24 18:42:07 +0200

    add IRecentList implementation (RecentList)

---
+++ RecentlyUsedList.Tests/RecentList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RecentlyUsedList
+{
+	public class RecentList : IRecentList
+	{
+		private List<string> _recentlyUsedList;
+		private readonly byte _maxCapacity = 5;
+
+		public RecentList()
+		{
+			_recentlyUsedList = new List<string>(_maxCapacity);
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			foreach (var item in _recentlyUsedList)
+			{
+				yield return item;
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public void Add(string value)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string LookUpByIndex(int index)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
