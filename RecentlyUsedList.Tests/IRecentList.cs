8caf1a11d1e8bc8d5012304792f7cbf495f10777
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-24 18:35:45 +0200

    add methods definition (Add, LookUpBuIndex) to IRecentList

--- RecentlyUsedList.Tests/IRecentList.cs
+++ RecentlyUsedList.Tests/IRecentList.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace RecentlyUsedList
 {
-	public interface IRecentList : IEnumerable
+	public interface IRecentList : IEnumerable<string>
 	{
-
+		public void Add(string value);
+		public string LookUpByIndex(int index);
 	}
 }
