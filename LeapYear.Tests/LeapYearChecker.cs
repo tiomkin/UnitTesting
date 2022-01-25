ad30169a949e9d248044ddd1b836342fd2abd06c
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-25 11:45:04 +0200

    modify Check() in LeapYearChecker to decrease nesting

--- LeapYear.Tests/LeapYearChecker.cs
+++ LeapYear.Tests/LeapYearChecker.cs
@@ -4,22 +4,22 @@
 	{
 		public bool Check(int year)
 		{
-			if (year % 4 == 0)
+			if (year % 4 != 0)
 			{
-				if (year % 100 == 0)
-				{
-					if (year % 400 == 0)
-					{
-						return true;
-					}
-
-					return false;
-				}
-
-				return true;
+				return false;
 			}
 
-			return false;
+			if (year % 100 == 0)
+			{
+				if (year % 400 == 0)
+				{
+					return true;
+				}
+
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
