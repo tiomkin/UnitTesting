1b1140066a5f2a01fbfdd68b78f00435ddf5ac37
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-25 11:52:07 +0200

    modify Check() in LeapYearChecker to simplify implementation

--- LeapYear.Tests/LeapYearChecker.cs
+++ LeapYear.Tests/LeapYearChecker.cs
@@ -4,22 +4,7 @@
 	{
 		public bool Check(int year)
 		{
-			if (year % 4 != 0)
-			{
-				return false;
-			}
-
-			if (year % 100 == 0)
-			{
-				if (year % 400 == 0)
-				{
-					return true;
-				}
-
-				return false;
-			}
-
-			return true;
+			return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 		}
 	}
 }
