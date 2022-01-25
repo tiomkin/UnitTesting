dc282362ba456915266d016e1048cafb692ee5ba
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-25 14:27:40 +0200

    add tests

--- FizzBuzz/PrinterTests.cs
+++ FizzBuzz/PrinterTests.cs
@@ -5,10 +5,60 @@ namespace FizzBuzz
 {
 	public class PrinterTests
 	{
-		[Fact]
-		public void Test1()
+		[Theory(Skip = "Interface not implemented")]
+		[InlineData(0)]
+		[InlineData(101)]
+		public void Print_AddOutOfRangeNumber_Exception(int number)
 		{
+			IPrinter printer = null;
 
+			Action result = () => printer.Print(number);
+
+			Assert.Throws<ArgumentOutOfRangeException>(result);
+		}
+
+		[Fact(Skip = "Interface not implemented")]
+		public void Print_NotDivisibleBy3AndNotDivisibledBy5_ReturnNumber()
+		{
+			var number = 2;
+			IPrinter printer = null;
+
+			var result = printer.Print(number);
+
+			Assert.Equal("2", result);
+		}
+
+		[Fact(Skip = "Interface not implemented")]
+		public void Print_DivisibleBy3AndNotDivisibledBy5_ReturnNumber()
+		{
+			var number = 3;
+			IPrinter printer = null;
+
+			var result = printer.Print(number);
+
+			Assert.Equal("Fizz", result);
+		}
+
+		[Fact(Skip = "Interface not implemented")]
+		public void Print_NotDivisibleBy3AndDivisibledBy5_ReturnNumber()
+		{
+			var number = 5;
+			IPrinter printer = null;
+
+			var result = printer.Print(number);
+
+			Assert.Equal("Buzz", result);
+		}
+
+		[Fact(Skip = "Interface not implemented")]
+		public void Print_DivisibleBy3AndDivisibledBy5_ReturnNumber()
+		{
+			var number = 15;
+			IPrinter printer = null;
+
+			var result = printer.Print(number);
+
+			Assert.Equal("FizzBuzz", result);
 		}
 	}
 }
