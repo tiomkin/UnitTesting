04f1a85270f683004a56a2b0099166418f960075
Artem Burdukov <artsiomburdukou@gmail.com>
2022-01-25 11:18:15 +0200

    add ILeapYearChecker implementation

---
+++ LeapYear.Tests/LeapYearChecker.cs
@@ -0,0 +1,25 @@
+﻿namespace LeapYear
+{
+	public class LeapYearChecker : ILeapYearChecker
+	{
+		public bool Check(int year)
+		{
+			if (year % 4 == 0)
+			{
+				if (year % 100 == 0)
+				{
+					if (year % 400 == 0)
+					{
+						return true;
+					}
+
+					return false;
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
